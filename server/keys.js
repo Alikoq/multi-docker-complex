91cb928acc067bc542e59bba6f58500b72d25b57
Alikoq <aquliyev75@gmail.com>
2022-01-16 16:09:22 +0100

    init

---
+++ server/keys.js
@@ -0,0 +1,10 @@
+module.exports={
+    redisHost: process.env.REDIST_HOST,
+    redisPort: process.env.REDIST_PORT,
+    pgUser: process.env.PGUSER,
+    pgHOST: process.env.PGHOST,
+    pgDatabase:  process.env.PGDATABASE,
+    pgPassword:  process.env.PGPASSWORD,
+    pgPort:  process.env.PGPORT
+
+}
